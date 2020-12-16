57c5d51da8819510a256a5a7c1f5c58de80445c1
nickschmitt <nickschmitt@gmail.com>
2020-12-16 12:11:40 -0800

    database migration

---
+++ migrations/2020-12-16-200844_diagrams/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE diagrams (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    photo TEXT NOT NULL,
+    caption TEXT NOT NULL,
+    topic VARCHAR NOT NULL,
+)
---
+++ src/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    diagrams (id) {
+        id -> Int4,
+        title -> Varchar,
+        photo -> Text,
+        caption -> Text,
+    }
+}
