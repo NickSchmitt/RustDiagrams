41f4017302ad7adf79bd1c2df5912478c6baf122
nickschmitt <nickschmitt@gmail.com>
2020-12-16 12:17:12 -0800

    models

---
+++ src/diagrams.rs
@@ -0,0 +1,12 @@
+// Handlebars
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+// queries
+use diesel::prelude::*;
+
+// macros
+use crate::schema::*;
+
+// structs
+use crate::models::*;
---
+++ src/models.rs
@@ -0,0 +1,21 @@
+// macros
+use crate::schema::*;
+
+// serialize
+use serde::Serialize;
+
+#[derive(Debug, Queryable, Serialize)]
+pub struct Diagram {
+    pub id: i32,
+    pub title: String,
+    pub photo: String,
+    pub caption: String,
+}
+
+#[derive(Debug, Insertable, AsChangeset)]
+#[table_name = "diagrams"]
+pub struct NewDiagram<'a> {
+    pub name: &'a str,
+    pub photo: String,
+    pub caption: String,
+}
